ce5207856a92850277d9129702560e979a3ab97d
Tanvir Hossain <tanvir22999629@gmail.com>
2026-06-26 11:54:33 +0600

    fix fucntional error

--- Task_101.c
+++ Task_101.c
@@ -1,102 +1,98 @@
 #include <SDL2/SDL.h>
-#include<bits/stdc++.h>
-using namespace std;
-// Defining  screen
+#include <stdio.h>
+
+// Defining screen
 #define SCREEN_WIDTH 1000
 #define SCREEN_HEIGHT 600
 
-// Global varible use
-bool gameIsRunning = false; // To control program is running or not
-SDL_Window* window = NULL;  // Main window
-SDL_Renderer* renderer = NULL; // Drawing tool
+// Global variables
+int gameIsRunning = 0; // To control program
+SDL_Window *window = NULL;
+SDL_Renderer *renderer = NULL;
 
 // Circle properties
-int centerX; // X coordinate of center of circle
-int centerY; // Y coordinate of center of circle
-int radius = 1000/5; // Radius of a cirle
+int centerX;
+int centerY;
+int radius = SCREEN_WIDTH / 5; // Radius of circle
 
-bool initializeWindow() // Window intialize Funtion and three type of working
+// Initialize window & renderer
+int initializeWindow()
 {
-    if (SDL_Init(SDL_INIT_VIDEO) < 0)   // Work 1: SDL starting to continue graphic system
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
     {
-        cout << "SDL failed to initialize\n";
-        return false;
+        printf("SDL failed to initialize\n");
+        return 0;
     }
 
     window = SDL_CreateWindow(
-        "Hello! this is my circle",      
+        "Hello! this is my circle",
         SDL_WINDOWPOS_UNDEFINED,
-        SDL_WINDOWPOS_UNDEFINED,      //Work 2: Creat a window
+        SDL_WINDOWPOS_UNDEFINED,
         SCREEN_WIDTH,
         SCREEN_HEIGHT,
         0);
 
     if (!window)
     {
-        cout << "Failed to open window\n";  
-        return false;
+        printf("Failed to open window\n");
+        return 0;
     }
 
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED); // work 3: creat a renderer
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     if (!renderer)
     {
-        cout << "Failed to create renderer\n";
-        return false;
+        printf("Failed to create renderer\n");
+        return 0;
     }
 
-    // Center of window
     centerX = SCREEN_WIDTH / 2;
     centerY = SCREEN_HEIGHT / 2;
 
-    return true;
+    return 1;
 }
 
+// Handle input
 void process_input()
 {
     SDL_Event event;
-
     while (SDL_PollEvent(&event))
     {
-        switch (event.type)
-        {
-        case SDL_QUIT:
-            gameIsRunning = false;
-            break;
-
-        default:
-            break;
-        }
+        if (event.type == SDL_QUIT)
+            gameIsRunning = 0;
     }
 }
 
 // Function to draw circle
 void drawCircle(int cx, int cy, int r)
 {
-    //scan squar area for circle points draw by using renderer drawing tool
- for(int i = -r; i<=r; i++){
-    for(int j = -r; j<=r; j++){
-        if( i*i + j*j <= r * r){
-            SDL_RenderDrawPoint(renderer, cx+i , cy + j);
+    for (int i = -r; i <= r; i++)
+    {
+        for (int j = -r; j <= r; j++)
+        {
+            if (i * i + j * j <= r * r)
+            {
+                SDL_RenderDrawPoint(renderer, cx + i, cy + j);
+            }
         }
     }
- }
 }
 
+// Draw everything
 void draw()
 {
-    // Background color Green
-    SDL_SetRenderDrawColor(renderer, 0, 103, 71,0);
+    // Background color: Green
+    SDL_SetRenderDrawColor(renderer, 0, 103, 71, 255);
     SDL_RenderClear(renderer);
 
-    // Circle color Red
-    SDL_SetRenderDrawColor(renderer, 218, 41, 28, 0);
+    // Circle color: Red
+    SDL_SetRenderDrawColor(renderer, 218, 41, 28, 255);
 
     drawCircle(centerX, centerY, radius);
 
-    SDL_RenderPresent(renderer); // Slow screen
+    SDL_RenderPresent(renderer);
 }
 
-// Closing renderer drawing tool
+// Cleanup SDL
 void destroyWindow()
 {
     SDL_DestroyRenderer(renderer);
@@ -104,7 +100,7 @@ void destroyWindow()
     SDL_Quit();
 }
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     gameIsRunning = initializeWindow();
 
@@ -115,6 +111,5 @@ int main(int argc, char* argv[])
     }
 
     destroyWindow();
-
     return 0;
 }
